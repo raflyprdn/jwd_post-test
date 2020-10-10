4fef16eedc5c892599cf1d1ef0b067894a3be61f
Rafly Pradana Putra <raflypradana31@gmail.com>
2020-10-10 18:18:35 +0700

    initial commit

---
+++ app/Controllers/Kamutube.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Kamutube extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    //--------------------------------------------------------------------
+
+}
